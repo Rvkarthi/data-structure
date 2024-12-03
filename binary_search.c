16e42817d1190fc76a20f14967b95fa9db8a801b
Rvname <karthikrishna465@gmail.com>
2024-12-03 11:30:34 +0530

    bs tree

---
+++ binary_search.c
@@ -0,0 +1,72 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* createnode(int data)
+{
+    struct node *n;
+    n=(struct node*)malloc(sizeof(struct node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+
+struct node* insert(struct node* root, int data)
+{
+    if(root==NULL)
+    {
+        return createnode(data);
+    }
+    else
+    {
+        if(data<root->data)
+        {
+            root->left = insert(root->left, data);
+        }
+        else if(data>root->data)
+        {
+            root->right = insert(root->right, data);
+        }
+    }
+    return root;
+}
+
+void inorderTraversal(struct node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+
+int main() {
+    
+    struct node* root = NULL;
+    // Insert nodes into the BST
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+    
+    printf("Inorder traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
+
+
+
+
